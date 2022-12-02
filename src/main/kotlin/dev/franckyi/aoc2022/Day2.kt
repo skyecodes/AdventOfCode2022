83fbf82d2ad9c2839ad57c86f6c6ca9c4e55ca35
Franck Velasco <franck.velasco@hotmail.fr>
2022-12-02 09:51:14 +0100

    day 2

---
+++ src/main/kotlin/dev/franckyi/aoc2022/Day2.kt
@@ -0,0 +1,9 @@
+package dev.franckyi.aoc2022
+
+fun main() {
+    val input = Utils.read(2).lines()
+    println(compute(input, listOf("B X", "C Y", "A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z")))
+    println(compute(input, listOf("B X", "C X", "A X", "A Y", "B Y", "C Y", "C Z", "A Z", "B Z")))
+}
+
+fun compute(input: List<String>, scores: List<String>): Int = input.sumOf { scores.indexOf(it) + 1 }
