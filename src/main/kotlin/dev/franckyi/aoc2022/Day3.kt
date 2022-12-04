18fd8c9450c6fe75c458b47f2f8fb60a198f9c6b
Franckyi <franck.velasco@hotmail.fr>
2022-12-04 19:01:36 +0100

    day 3

---
+++ src/main/kotlin/dev/franckyi/aoc2022/Day3.kt
@@ -0,0 +1,17 @@
+package dev.franckyi.aoc2022
+
+fun main() {
+    val lines = Utils.read(3).lines()
+    println(part1(lines))
+    println(part2(lines))
+}
+
+fun part1(lines: List<String>) = lines.map { it.substring(0, it.length / 2) to it.substring(it.length / 2) }
+        .map { (a, b) -> a.find { it in b }!!.code }
+        .sumOf { getPriority(it) }
+
+fun part2(lines: List<String>) = lines.chunked(3)
+        .map { (a, b, c) -> a.find { it in b && it in c }!!.code }
+        .sumOf { getPriority(it) }
+
+fun getPriority(char: Int) = if (char > 90) char % 96 else char % 64 + 26
